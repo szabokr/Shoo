eff61843426f4f33ae39a9d6b017b5311f2adf1f
Nagy Robert <robi20010713@gmail.com>
2023-03-21 20:58:25 +0100

    Implemented a few view templates.

---
+++ app/views/landing_page/landingPage.php
@@ -0,0 +1,218 @@
+    <!-- Main Page's Start -->
+    <!-- Welcome's Start -->
+    <section>
+        <div class="maingraddient pt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xxl-5 col-xl-5 col-lg-5 border border-1 rounded border-opacity-50 shadow bg-light">
+                        <h1 class="display-6 text-uppercase pt-3 text-center text-muted">Welcome at Shoosh!</h1>
+                        <p class="fw-light fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque fugiat
+                            vero
+                            esse.
+                            Unde incidunt
+                            excepturi recusandae enim, perspiciatis soluta delectus nisi explicabo voluptatibus saepe
+                            voluptates
+                            itaque id tenetur quibusdam in.<br><br>
+                            Mollitia rerum ab quos reiciendis aperiam
+                            illo, ipsam at velit nostrum quasi debitis nulla omnis et reprehenderit quidem, quisquam
+                            autem.
+                            Et,
+                            enim?</p>
+                    </div>
+
+                    <div
+                        class="col-xxl-2 col-xl-2 col-lg-2 d-flex text-muted align-items-center justify-content-center">
+                        <i class="bi bi-geo iconhover" style="font-size: 5rem;"></i>
+                    </div>
+
+                    <div class="col-xxl-5 col-xl-5 col-lg-5 border border-1 rounded border-opacity-50 shadow bg-light">
+                        <h1 class="display-6 text-uppercase pt-3 text-center text-muted">Location of our office</h1>
+                        <div class="ratio ratio-16x9 mb-3">
+                            <iframe
+                                class="position-absolute top-50 start-50 translate-middle border border-1 rounded shadow h-100"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d170.44007164471694!2d19.648071155190042!3d46.88223908185364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743d07921200811%3A0x790e4eb26664c896!2sKecskem%C3%A9t%2C%20Homokszem%20utca%203%2C%206000!5e0!3m2!1shu!2shu!4v1679094239755!5m2!1shu!2shu"
+                                loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Welcome's End -->
+
+    <!-- Banner's Start -->
+    <section>
+        <div class="container bg-light">
+            <div class="row my-5">
+                <div class="col-12 border border-1 rounded border-opacity-75 shadow">
+                    <div class="row">
+                        <h1 class="display-6 text-uppercase text-center text-muted fs-3 py-3">Our Partners:</h1>
+                    </div>
+                    <div class="row align-items-center text-center text-muted">
+                        <div class="col-6 col-lg-3">
+                            <div class="image">
+                                <a href="adidas.com"><img src="brands/adidas-logo.png" class="img-fluid w-75 p-2 p-lg-0 pb-lg-4" alt="adidas logo"></a>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-3">
+                            <div class="image">
+                                <a href="newbalance.com"><img src="brands/nb-logo.png" class="img-fluid w-75 p-2 p-lg-0 pb-lg-4" alt="newbalance logo"></a>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-3">
+                            <div class="image">
+                                <a href="nike.com"><img src="brands/nike-logo.png" class="img-fluid w-75 p-2 p-lg-0 pb-lg-4" alt="nike logo"></a>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-3">
+                            <div class="image">
+                                <a href="thenorthface.com"><img src="brands/tnf-logo.png" class="img-fluid w-75 p-2 p-lg-0 pb-lg-4" alt="tnf logo"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Banner's End -->
+
+    <!-- Top Shoes's Start -->
+    <section>
+
+        <div class="container bg-light border border-1 rounded border-opacity-75 shadow my-5">
+            <div class="row">
+                <!-- <div class="col"> -->
+                <h1 class="display-6 text-uppercase text-center text-muted fs-3 py-3">Our best sellers:</h1>
+                <!-- </div> -->
+            </div>
+            <div class="row">
+                <div class="col text-center pb-5 d-flex justify-content-center">
+                    <div class="card carditem shadow" style="width: 18rem;">
+                        <img src="shoes/adidas30.JPEG" class="object-fit-cover border rounded card-img-tp"
+                            style="width: 18em; height: 15em;" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">$shoename</h5>
+                            <p class="card-text">some text here</p>
+                            <a href="#" class="btn btn-outline-success">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col text-center pb-5 d-flex justify-content-center">
+                    <div class="card carditem shadow" style="width: 18rem;">
+                        <img src="shoes/adidas1.JPEG" class="object-fit-cover border rounded card-img-tp"
+                            style="width: 18em; height: 15em;" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">$shoename</h5>
+                            <p class="card-text">some text here</p>
+                            <a href="#" class="btn btn-outline-success">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col text-center pb-5 d-flex justify-content-center">
+                    <div class="card carditem shadow" style="width: 18rem;">
+                        <img src="shoes/adidas15.JPEG" class="object-fit-cover border rounded card-img-tp"
+                            style="width: 18em; height: 15em;" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">$shoename</h5>
+                            <p class="card-text">some text here</p>
+                            <a href="#" class="btn btn-outline-success">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col text-center pb-5 d-flex justify-content-center">
+                    <div class="card carditem shadow" style="width: 18rem;">
+                        <img src="shoes/adidas34.JPEG" class="object-fit-cover border rounded card-img-tp"
+                            style="width: 18em; height: 15em;" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">$shoename</h5>
+                            <p class="card-text">some text here</p>
+                            <a href="#" class="btn btn-outline-success">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Top Shoes's End -->
+    <!-- Banner's Start -->
+    <section>
+        <div class="container bg-light">
+            <div class="row my-5">
+                <div class="col-12 border border-1 rounded border-opacity-75 shadow">
+                    <div class="row align-items-center text-center text-muted">
+                        <div class="col">
+                            <i class="bi bi-geo-alt" style="font-size: 3.5rem;"></i>
+                            <h1 class="display-6 fs-3 text-uppercase mt-1">Free collection<br>in our stores</h1>
+                            <p class="fw-light fs-5">Regardless of amount<br>and product!</p>
+                        </div>
+                        <div class="col">
+                            <i class="bi bi-truck" style="font-size: 3.5rem;"></i>
+                            <h1 class="display-6 fs-3 text-uppercase mt-1">nationwide delivery<br>from $8,99</h1>
+                            <p class="fw-light fs-5">Regardless of package<br>size and weight!</p>
+                        </div>
+                        <div class="col">
+                            <i class="bi bi-recycle" style="font-size: 3.5rem;"></i>
+                            <h1 class="display-6 fs-3 text-uppercase mt-1">3 day return<br>guarantee</h1>
+                            <p class="fw-light fs-5">In case of damaged<br>or wrong sized product.</p>
+                        </div>
+                        <div class="col">
+                            <i class="bi bi-postcard-heart" style="font-size: 3.5rem;"></i>
+                            <h1 class="display-6 fs-3 text-uppercase mt-1">loyalty program<br>for our customers</h1>
+                            <p class="fw-light fs-5">Promotions and discounts<br>every month!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Banner's End -->
+    <!-- Newsletter's Start -->
+    <section>
+        <div class="container shadow text-muted bg-light">
+            <div class="row my-5">
+                <div class="col-12 border border-1 rounded border-opacity-50">
+                    <div class="row align-items-center justify-content-center">
+                        <div class="col-xxl-1 col-xl-1 col-lg-1 text-center opacity-75"><i
+                                class="bi bi-envelope-paper-heart-fill" style="font-size: 5.5rem;"></i>
+                        </div>
+                        <div class="col-xxl-4 col-xl-4 col-lg-4 text-center text-uppercase fs-4 fw-bold opacity-75">
+                            Subscribe to our
+                            Newsletter!</div>
+
+                        <div class="col-xxl-4 col-xl-4 col-lg-4 m-4 text-center">
+
+                            <form class="d-inline-flex opacity-75 ">
+                                <div class="col-lg-5 me-3">
+                                    <div class="form-outline d-flex">
+                                        <label for="firstName">
+                                            <i class="bi bi-person-fill mx-1" style="font-size:2rem;"></i>
+                                        </label>
+                                        <input type="text" id="firstName"
+                                            class="form-control form-control-md fs-5 shadow" placeholder="Name"
+                                            required />
+                                    </div>
+                                </div>
+                                <div class="col-lg-7">
+                                    <div class="form-outline d-flex">
+                                        <label for="email"><i class="bi bi-envelope-fill mx-1"
+                                                style="font-size:2rem;"></i>
+                                        </label>
+                                        <input type="email" id="email" class="form-control form-control-md fs-5 shadow"
+                                            placeholder="john@email.com" required />
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+
+                        <div class="col-xxl-1 col-xl-1 col-lg-1 mb-lg-0 mb-md-4 mb-sm-4 mb-xs-4 text-center"><button
+                                class="btn btn-outline-success btn-block fs-5 shadow" type="submit">Subsrcibe!</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Newsletter's End -->
+
+    <!-- Main Page's End -->
